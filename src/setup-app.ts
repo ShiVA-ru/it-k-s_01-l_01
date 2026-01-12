ce5971cc41c1a8211c8c27c674314bd404d130e7
Shinyaev Vladimir <secman@bk.ru>
2026-01-12 17:29:33 +0500

    add swagger docs

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { HttpStatus } from "./core/types/http-statuses";
 import { driversRouter } from "./drivers/drivers.router";
 import { testingRouter } from "./testing/testing.router";
+import { setupSwagger } from "./core/swagger/setup-swagger";
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
@@ -12,6 +13,8 @@ export const setupApp = (app: Express) => {
 
   app.use("/drivers", driversRouter);
   app.use("/testing", testingRouter);
+
+  setupSwagger(app);
 
   return app;
 };
